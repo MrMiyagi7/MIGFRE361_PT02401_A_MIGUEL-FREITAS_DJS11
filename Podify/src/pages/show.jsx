9bcb8b911ee02a18848b10e595ebcf26e2437985
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-11-04 18:46:28 +0200

    Created ShowDetails function and extract id from the url using useParams hook

--- Podify/src/pages/show.jsx
+++ Podify/src/pages/show.jsx
@@ -1,3 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+
+export default function ShowDetails() {
+  const { id } = useParams(); // Extract show ID from URL parameters
+}
