b3ffffd8dffaa05de241d421c12d2e3ecd0dcf1b
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-11-04 11:37:14 +0200

    imported hooks to be used in home page

--- Podify/src/pages/home.jsx
+++ Podify/src/pages/home.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default function Home() {
-  return <h1>This is the home page</h1>;
+  return;
 }
