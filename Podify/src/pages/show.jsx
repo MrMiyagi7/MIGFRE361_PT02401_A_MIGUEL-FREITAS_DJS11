ee4b5abee80c71e5985107c6442de6c1273d9547
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-11-04 19:18:06 +0200

    added styling for individual show page

--- Podify/src/pages/show.jsx
+++ Podify/src/pages/show.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import "../show.css";
 
 export default function ShowDetails() {
   const { id } = useParams(); // Extract show ID from URL parameters
