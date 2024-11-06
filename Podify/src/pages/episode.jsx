22cd0a935db149953ff787dcc38319e47fe7fffb
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-11-06 14:50:00 +0200

    Fetch data show data from api and use logic to retrieve episode details

--- Podify/src/pages/episode.jsx
+++ Podify/src/pages/episode.jsx
@@ -7,3 +7,19 @@ export default function EpisodeDetails() {
   const { id, seasonNumber, episodeId } = useParams();
   const [episode, setEpisode] = useState(null);
   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchEpisode = async () => {
+      try {
+        const response = await axios.get(
+          `https://podcast-api.netlify.app/id/${id}`
+        );
+        const showDetails = response.data;
+        const selectedSeason = showDetails.seasons.find(
+          (season) => season.season === parseInt(seasonNumber)
+        );
+        const selectedEpisode = selectedSeason.episodes.find(
+          (ep) => ep.episode === parseInt(episodeId)
+        );
+
+        setEpisode(selectedEpisode);
