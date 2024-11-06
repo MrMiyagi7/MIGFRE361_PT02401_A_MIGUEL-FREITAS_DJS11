2f03298ad187bc3454e51261c64915c5c40d4abe
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-11-06 14:46:51 +0200

    intialised id, seasonNumber and episode id with useParams hook

--- Podify/src/pages/episode.jsx
+++ Podify/src/pages/episode.jsx
@@ -2,3 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { Link } from "react-router-dom";
+
+export default function EpisodeDetails() {
+  const { id, seasonNumber, episodeId } = useParams();
