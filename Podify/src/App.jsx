7461cea0a3e79436fe3b8d431996f93abe280ceb
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-11-06 14:53:40 +0200

    Added epsiode component route

--- Podify/src/App.jsx
+++ Podify/src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Layout from "../components/layout";
 import Home from "./pages/home";
 import ShowDetails from "./pages/show";
+import EpisodeDetails from "./pages/episode";
 
 function App() {
   return (
@@ -12,6 +13,10 @@ function App() {
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/show/:id" element={<ShowDetails />} />
+          <Route
+            path="/show/:id/season/:seasonNumber/episode/:episodeId"
+            element={<EpisodeDetails />}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
