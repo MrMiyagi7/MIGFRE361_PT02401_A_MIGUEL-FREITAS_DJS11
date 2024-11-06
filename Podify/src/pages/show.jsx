dc105f14ab1095e716b3cd5d319b147eef90322b
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-11-06 15:21:28 +0200

    initialised state of show favourite status

--- Podify/src/pages/show.jsx
+++ Podify/src/pages/show.jsx
@@ -1,15 +1,14 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
 import { useParams, Link } from "react-router-dom";
 import axios from "axios";
 import "../show.css";
 
 export default function ShowDetails() {
-  const { id } = useParams(); // Extract show ID from URL parameters
   const { id } = useParams();
   const [showDetails, setShowDetails] = useState(null);
   const [error, setError] = useState(null);
   const [expandedSeasons, setExpandedSeasons] = useState({});
+  const [isFavourite, setIsFavourite] = useState(false);
 
   useEffect(() => {
     const fetchShowDetails = async () => {
