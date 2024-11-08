b6ca97d41b8091fa079179488eb5e8fa2d42bd12
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-11-08 11:59:54 +0200

    Added function to handle variable for audio player

--- Podify/components/layout.jsx
+++ Podify/components/layout.jsx
@@ -8,6 +8,13 @@ export default function Layout() {
   const [currentAudio, setCurrentAudio] = useState(null);
   const [audioTitle, setAudioTitle] = useState("");
   const [season, setSeason] = useState("");
+
+  const handleAudioChange = (audioUrl, title, season) => {
+    setCurrentAudio(audioUrl);
+    setAudioTitle(title);
+    setSeason(season);
+  };
+
   return (
     <div className="site-wrapper">
       <Header />
