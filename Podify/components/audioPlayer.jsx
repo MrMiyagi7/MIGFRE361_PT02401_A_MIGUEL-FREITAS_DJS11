b4501ef1c3fbc2359f71417ed65a3c5045f7f371
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-11-08 11:40:30 +0200

    Added function to handle play and pause control button

--- Podify/components/audioPlayer.jsx
+++ Podify/components/audioPlayer.jsx
@@ -4,3 +4,12 @@ export default function AudioPlayer({ currentAudio, title, season }) {
   const audioRef = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
   const [progress, setProgress] = useState(0);
+
+  const togglePlayPause = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
