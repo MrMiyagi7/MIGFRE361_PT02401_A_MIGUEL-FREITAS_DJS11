777f8cb7630552b7c3e301c310691e422ade3ada
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-11-08 11:54:49 +0200

    Import auido player into layout component

--- Podify/components/layout.jsx
+++ Podify/components/layout.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./header";
 import Footer from "./footer";
+import AudioPlayer from "./audioPlayer";
 
 export default function Layout() {
   return (
