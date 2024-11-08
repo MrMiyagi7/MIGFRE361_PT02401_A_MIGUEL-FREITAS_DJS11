fd39e1a3dbbcd74db7c6a7e6d679ba41d9be2a18
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-11-08 18:16:36 +0200

    Added link to favourite page in header component

--- Podify/components/header.jsx
+++ Podify/components/header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import darklogo from "../src/assets/dark-logo.png";
 import lightlogo from "../src/assets/light-logo.png";
+import heart from "../src/assets/heart.svg";
 
 export default function Header() {
   return (
@@ -15,6 +16,11 @@ export default function Header() {
           type="text"
           placeholder="What show are you looking for?"
         ></input>
+        <Link to="/favourites">
+          <p className="favourite-link">
+            My favourites <img className="heart-icon" src={heart}></img>
+          </p>
+        </Link>
         <span className="user-icon">M</span>
       </nav>
     </header>
