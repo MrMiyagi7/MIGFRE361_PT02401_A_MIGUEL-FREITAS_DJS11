6f055f6d37481a5ea5a2c8f73788faa38b985fa9
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-11-08 12:02:44 +0200

    import kink and useOutletContext hooks

--- Podify/src/pages/episode.jsx
+++ Podify/src/pages/episode.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link, useOutletContext } from "react-router-dom";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
