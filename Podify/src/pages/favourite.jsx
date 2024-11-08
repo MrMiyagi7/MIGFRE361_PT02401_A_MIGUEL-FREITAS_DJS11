bd82862823b3cfea25a24637d1d674ddfd1627b6
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-11-08 16:28:01 +0200

    updated favourite component to match favourite episodes not shows

--- Podify/src/pages/favourite.jsx
+++ Podify/src/pages/favourite.jsx
@@ -13,18 +13,24 @@ export default function Favourites() {
   if (favourites.length === 0) {
     return <p>No favourites added yet.</p>;
   }
+  console.log(favourites);
 
   return (
     <div className="favourites">
-      <h1>Your Favourite Shows</h1>
+      <h1>Your Favourite Episodes</h1>
       <div className="podcast-previews">
-        {favourites.map((show) => (
-          <Link to={`/show/${show.id}`} key={show.id}>
+        {favourites.map((episode) => (
+          <Link
+            to={`/show/${episode.showId}/season/${episode.seasonNumber}/episode/${episode.episodeId}`}
+            key={episode.episodeId}
+          >
             <div className="podcast-card">
               <div className="podcast-info">
-                <img src={show.image} alt={`${show.title} cover`} />
-                <h3>{show.title}</h3>
-                <p className="podcast-description">{show.description}</p>
+                <img src={episode.image} alt={`${episode.title} cover`} />
+                <h3>{episode.title}</h3>
+                <p className="podcast-description">{episode.description}</p>
+                <p>{episode.showTitle}</p>
+                <p>Episode {episode.episodeId}</p>
               </div>
             </div>
           </Link>
